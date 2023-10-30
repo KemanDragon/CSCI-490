93b3fe838734533672d89dd0d6002f0a07df93d1
Samuel Cannon <keman.wicker@gmail.com>
2023-10-30 01:29:42 -0400

    Updated Program.cs to contain initial bot setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program {
+    public static Task Main(string[] args) => new Program().MainAsync();
+    private DiscordSocketClient _client;
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task MainAsync() {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var token = File.ReadAllText("token.txt");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
