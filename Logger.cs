20f6da69b75ab04d00f20ab36d8024cedccdd286
camorri4 <camorri4@coastal.edu>
2023-10-30 14:50:47 -0400

    Made changes and added logic to offensive language handler

--- Logger.cs
+++ Logger.cs
@@ -6,19 +6,22 @@ using Discord.Net;
 using Discord.Rest;
 using Discord.WebSocket; //Since Discord bots use Sockets in order to be able to connect to different servers, I decided to add it here
 using MySQL.Data;
+using MySQL.Data.MySQLClient;
 
 
 public class logger
 {
     private readonly DiscordSocketClient _client;
-    private Logger _logger;
+    private MySqlConnection _connection;
+    //private Logger _logger;
 
     public Logger()
     {
         _client = new DiscordSocketClient();
         _client.Log += LogAsync;
-        
-       
+        _connectionString = connectionString;
+
+
         // Add event asyncs
         _client.UserBanned += UserBannedAsync;
         _client.MessageReceived += MessageReceivedAsync;
@@ -32,8 +35,18 @@ public class logger
     {
         if (message is SocketUserMessage userMessage)
         {
-            // You can add logic to check for offensive language here
-            // If offensive language is detected, call _logger.LogOffensiveLanguage
+            // Check for offensive language in the message content
+            if (ContainsOffensiveLanguage(userMessage.Content))
+            {
+                // Log the event using the Logger
+                _logger.LogOffensiveLanguage(userMessage.Author.Id, userMessage.Content);
+
+                // Optionally, you can delete the offending message
+                await userMessage.DeleteAsync();
+
+                // You can also send a warning or take other actions as needed
+                await message.Channel.SendMessageAsync($"@{message.Author.Username}, please refrain from using offensive language.");
+            }
         }
         return Task.CompletedTask;
     }
@@ -59,5 +72,37 @@ public class logger
                                                           // and the reason for which the user was banned.
         return Task.CompletedTask; //Task has been completed.
     }
+
+
+
+
+
+    private void ConnectToDatabase()
+    {
+        try
+        {
+            _connection = new MySqlConnection(_connectionString);
+            _connection.Open();
+            Console.WriteLine("Connected to the database successfully.");
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine("Error connecting to the database: " + ex.Message);
+        }
+    }
+
+    // Other Discord event handlers
+    private Task UserBannedAsync(SocketUser user, SocketGuild guild, string reason)
+    {
+        // Perform database operations related to banned users
+        // e.g., INSERT INTO BannedUsers (UserID, GuildID, Reason) VALUES (user.Id, guild.Id, reason);
+        return Task.CompletedTask;
+    }
+
+  
+
+   
+    
+    
 
 }
